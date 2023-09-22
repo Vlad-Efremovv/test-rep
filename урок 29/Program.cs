7a4adf3c10f6849aabf846639a1986b91acd1b37
Efremov <vlade.efrrvov@mail.ru>
2023-09-22 22:49:16 +0300

    New commit 22.09.2023

---
+++ урок 29/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace урок_29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] data = { { 1, 2, 3, 4, 5 }
+                , { 1, 2, 3, 4, 5}
+                , { 1, 2, 3, 4, 5}};
+
+
+
+
+            Console.WriteLine(data[1,3]);
+        }
+    }
+}
